1cc9797ead9d2ac4a168c97b38817267f942a088
Florisvaldo <juniortcj10012@hotmail.com>
2024-01-13 20:10:47 -0300

    Programa para escrever nome

---
+++ Teste.java
@@ -0,0 +1,7 @@
+import java.util.Locale; // Para usar um ponto como separador decimal
+
+public class Teste {
+    public static void main(String[] args) {
+        System.out.print("Bom dia");
+    }
+}
